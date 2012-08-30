5a9d2df662c7ea29bba2844b422569b0ca1e0d3f
Matt Owen <mattisbusiness@gmail.com>
2012-08-30 16:01:23 -0400

    Fin.

--- R/plot.pooled.sim.R
+++ R/plot.pooled.sim.R
@@ -3,19 +3,31 @@
 #' Plot pooled simulated quantities of interest.
 #' @usage \method{plot}{pooled.sim}(x, xlab = "", ...)
 #' @S3method plot pooled.sim
-#' @param x a `sim' object
-#' @param CI a number, between 0 and 100, specifying the confidence interal to
+#' @param x A `sim' object
+#' @param CI A number, between 0 and 100, specifying the confidence interal to
 #' construct
-#' @param xlab labels for the x-axis
-#' @param ... parameters to be passed to the `truehist' function which is 
+#' @param xlab Labels for the x-axis
+#' @param ... Parameters to be passed to the `truehist' function which is 
 #' implicitly called for numeric simulations
+#' @param leg.pos ``legend type'', exact coordinates and sizes for legend.
+#' Overrides argment ``leg.type''
+#' @param leg.type ``legend position'', an integer from 1 to 4, specifying the
+#' position of the legend. 1 to 4 correspond to ``SE'', ``SW'', ``NW'', and
+#' ``NE'' respectively
+#' @param leg.col ``legend color'', an valid color used for plotting the line
+#' colors in the legend
 #' @return the current graphical parameters. This is subject to change in future
 #' implementations of Zelig
 #' @author James Honaker, adapted by Matt Owen \email{mowen@@iq.harvard.edu}
-plot.pooled.sim <- function (x, CI = 95, col = NULL, qi = "Expected Values", xlab = "", ...) {
+plot.pooled.sim <- function (x, CI = 95, col = NULL, qi = "Expected Values", xlab = "", ..., leg.pos = NULL, leg.type = 1, leg.col = "gray30") {
 
+  # Specify graphical parameters
+  par(bty = "n")
+
+  # Get the closest fit to an exact match for a quantity of interest
   qi <- find.match(qi, attr(x, "titles"))
 
+  # If there is no match at all, return an error
   if (is.na(qi)) {
     warning(
       "Warning the specified quantity of interest does not exist\n  Legal values are: ",
@@ -25,8 +37,8 @@ plot.pooled.sim <- function (x, CI = 95, col = NULL, qi = "Expected Values", xla
     invisible(NULL)
   }
     
-  # Specify the colors
-  if (missing(col))
+  # Specify the colors, if none are given
+  if (is.null(col))
     col <- c(
       rgb(100, 149, 237, alpha=50, maxColorValue=255),
       rgb(152, 245, 255, alpha=50, maxColorValue=255),
@@ -68,8 +80,8 @@ plot.pooled.sim <- function (x, CI = 95, col = NULL, qi = "Expected Values", xla
   qi.unique <- list()
 
   # Find out which column varies
-  for (col in colnames(X))
-    qi.unique[col] <- length( unique(X[, col]) ) > 1
+  for (column in colnames(X))
+    qi.unique[column] <- length( unique(X[, column]) ) > 1
 
   unique.column <- names(Filter(function (x) x == TRUE, qi.unique))
 
@@ -108,6 +120,77 @@ plot.pooled.sim <- function (x, CI = 95, col = NULL, qi = "Expected Values", xla
     history[k, "Lower 99.9%"] <- ci.lower(vals, .999)
   }
 
+  # Get limits for plot
+  plot.domain <- c(min(history[, 1]), max(history[, 1]))
+  plot.range <- c(min(history[, -1]), max(history[, -1]))
+  plot.label <- paste("Range of \"", unique.column, "\"", sep="")
+
   #
-  history
+  plot(
+    # Plot points on an x-y axis. Check out this semantic programming
+    history[, "Value"], history[, "Median"],
+    # Specify domain and range of plot
+    xlim = plot.domain, ylim = plot.range,
+    # Label the axes
+    xlab = plot.label, ylab = qi,
+    # Title the plot
+    main = "Confidence Intervals with Respect to Explanatory Variables"
+    )
+
+
+  # Values
+  k <- ncol(Y)
+  x.coords <- c(history[, 1], history[k:1, 1])
+
+  # Draw region around 80% confidence interval (NOTE: 3 = Upper 80%, 4 = Lower 80%)
+  polygon(x.coords, c(history[, 3], history[k:1, 4]), col=col[1], border="gray60")
+
+  # Draw region around 95% confidence interval (NOTE: 5 = Upper 80%, 6 = Lower 80%)
+  polygon(x.coords, c(history[, 5], history[k:1, 6]), col=col[2], border="gray90")
+
+  # Draw region around 99.9% confidence interval (NOTE: 7 = Upper 80%, 8 = Lower 80%)
+  polygon(x.coords, c(history[, 7], history[k:1, 8]), col=col[3], border="white")
+
+
+
+    ## This is the legend
+
+  if(is.null(leg.pos)) {
+    leg.pos <- if (leg.type == 1)
+      c(.91, .04, .2, .05)
+    else if (leg.type == 2)
+      c(.09, .04, .2, .05)
+    else if (leg.type == 3)
+      c(.09, .04, .8, .05)
+    else
+      c(.91, .04, .8, .05)
+  }
+
+  # Construct positions for legend
+  lx <- plot.domain[1] + leg.pos[1] * (plot.domain[2] - plot.domain[1])
+  hx <- plot.domain[1] + (leg.pos[1] + leg.pos[2]) * (plot.domain[2] - plot.domain[1])
+  deltax <- (hx - lx)/10
+  my <- plot.range[1] + leg.pos[3] * (plot.range[2] - plot.range[1])
+  dy <- leg.pos[4] * (plot.range[2] - plot.range[1])
+
+  # Draw the lines for the legend
+  lines(c(hx+deltax,hx+2*deltax,hx+2*deltax,hx+deltax),c(my+3*dy,my+3*dy,my-3*dy,my-3*dy),col=leg.col)
+  lines(c(hx+3*deltax,hx+4*deltax,hx+4*deltax,hx+3*deltax),c(my+1*dy,my+1*dy,my-1*dy,my-1*dy),col=leg.col)
+  lines(c(lx-deltax,lx-2*deltax,lx-2*deltax,lx-deltax),c(my+2*dy,my+2*dy,my-2*dy,my-2*dy),col=leg.col)
+  lines(c(lx-5*deltax,lx),c(my,my),col="white",lwd=3)
+  lines(c(lx-5*deltax,lx),c(my,my),col=leg.col)
+  lines(c(lx,hx),c(my,my))
+
+  # Color in the lines drawn for the legend
+  polygon(c(lx,lx,hx,hx),c(my-2*dy,my+2*dy,my+2*dy,my-2*dy),col=col[2],border="gray90")
+  polygon(c(lx,lx,hx,hx),c(my-1*dy,my+1*dy,my+1*dy,my-1*dy),col=col[1],border="gray60")
+  polygon(c(lx,lx,hx,hx),c(my-3*dy,my+3*dy,my+3*dy,my-3*dy),col=col[3],border="white")
+
+  # Label the legend
+  text(lx,my,labels="median",pos=2,cex=0.5,col=leg.col)
+  text(lx,my+2*dy,labels="ci95",pos=2,cex=0.5,col=leg.col)
+  text(hx,my+1*dy,labels="ci80",pos=4,cex=0.5,col=leg.col)
+  text(hx,my+3*dy,labels="ci99.9",pos=4,cex=0.5,col=leg.col)
+
+  # Fin.
 }
--- tests/plot.pooled.sim.R
+++ tests/plot.pooled.sim.R
@@ -3,7 +3,7 @@ library(Zelig)
 data(turnout)
 
 z <- zelig(vote ~ income + educate, model="logit", data=turnout)
-x <- setx(z, educate=10:14)
+x <- setx(z, educate=3:24)
 s <- sim(z, x)
 
 plot(s)
