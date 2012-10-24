7ff403c8543e0315b618eeb8ed825ab11c394954
Matt Owen <mattisbusiness@gmail.com>
2012-10-24 07:09:59 -0400

    Adding final models

---
+++ R/lognorm.R
@@ -0,0 +1,105 @@
+#' Interface between the Zelig Model lognorm and 
+#' the Pre-existing Model-fitting Method
+#' @param formula a formula
+#' @param ... additonal parameters
+#' @param data a data.frame 
+#' @return a list specifying '.function'
+#' @export
+zelig2lognorm <- function (formula, ..., robust = FALSE, cluster = NULL, data) {
+
+  if (!(is.null(cluster) || robust))
+    stop("If cluster is specified, then `robust` must be TRUE")
+
+  # Add cluster term
+  if (robust || !is.null(cluster))
+    formula <- cluster.formula(formula, cluster)
+
+  # Return
+  list(
+       .function = "survreg",
+       formula = formula,
+       dist = "lognormal",
+       robust = robust,
+       data = data,
+       ...
+       )
+}
+
+#' @S3method param lognorm
+param.lognorm <- function(obj, num=1000, ...) {
+
+  # These are the fitted parameters
+  coef <- coef(obj)
+
+  # Append the log-scale
+  mu <- c(coef, log(obj$result$scale))
+
+  # These are their correlations
+  cov <- vcov(obj)
+
+  # Simulate the results
+  simulations <- mvrnorm(num, mu, cov)
+
+  # Return
+  list(
+       coef = as.matrix(simulations[, 1:length(coef)]),
+       alpha = as.matrix(simulations[, -(1:length(coef))]),
+       linkinv = survreg.distributions[["lognormal"]]$itrans
+       )
+}
+
+#' @S3method qi lognorm
+qi.lognorm <- function(obj, x=NULL, x1=NULL, y=NULL, num=1000, param=NULL) {
+
+  linkinv <- linkinv(param)
+  alpha <- alpha(param)
+  beta <- coef(param)
+
+  # Compute expected values for "lognorm" regression
+  #
+  # This function is nested within qi.lognorm for code-clarity and because it
+  # will not be used by any other function
+  # @param coef
+  # @param alpha sim.scale
+  # @param x
+  # @return a matrix
+  compute.ev <- function (coef, alpha, x) {
+    if (is.null(x) || is.na(x))
+      # If there are missing explanatory variables, ignore them
+      return(NA)
+
+    # Compute eta
+    # This value must be *inverted* to be restored to the true "observed" value
+    eta <- coef %*% t(x)
+
+    # Apply inverse link function
+    theta <- as.matrix(apply(eta, 2, linkinv))
+
+    # Copied from qi.survreg in Zelig v3.5
+    ev <- exp(log(theta) + 0.5*(exp(alpha))^2)
+    dimnames(ev) <- dimnames(theta)
+
+    # Return
+    as.matrix(ev)
+  }
+
+  # Compute expected values for X and X1
+  ev1 <- compute.ev(beta, alpha, x)
+  ev2 <- compute.ev(beta, alpha, x1)
+
+
+  list(
+       "Expected Value: E(Y|X)" = ev1,
+       "Expected Value: E(Y|X1)" = ev2,
+       "First Differences: E(Y|X1) - E(Y|X)" = ev2 - ev1
+       )
+}
+
+#' @S3method describe lognorm
+describe.lognorm <- function(...) {
+  list(
+       authors = c("Matthew Owen", "Olivia Lau", "Kosuke Imai", "Gary King"),
+       text = "Log-Normal Regression for Duration Dependent Variables",
+       year = 2007
+       )
+}
---
+++ R/twosls.R
@@ -0,0 +1,195 @@
+#' Interface between the Zelig Model twosls and 
+#' the Pre-existing Model-fitting Method
+#' @param formula a formula
+#' @param ... additonal parameters
+#' @param data a data.frame 
+#' @return a list specifying '.function'
+#' @export
+zelig2twosls <- function (formula, ..., data) {
+
+  # Helper function to perform set-difference
+  "%w/o%" <- function(x, y)
+    x[!x %in% y]
+
+  formula<-parse.formula(formula, "twosls")
+  tt<-terms(formula)
+
+  ins<-names(tt) %w/o% names(attr(tt,"depVars"))
+  if(length(ins)!=0)
+    if(length(ins)==1)
+      inst <- formula[[ins]]
+    else 
+      inst <- formula[ins]
+
+  else
+    stop("twosls model requires instrument!!\n")
+
+  class(formula) <- c("multiple", "list")
+
+  # Return
+  list(
+       .function = "callsystemfit",
+       formula = formula[names(attr(tt,"depVars"))],
+       method  = "2SLS",
+       inst    = inst,
+       data = data,
+       ...
+       )
+}
+
+#' @S3method param twosls
+param.twosls <- function(obj, num=1000, ...) {
+
+  # Produce a vector of all terms
+  big.coef <- coef(obj)
+
+  # Produce a pretty sparse matrix containing 3 vcov matrices.
+  #
+  # Note that this matrix will give a value of zero to any invalid row-column
+  # combination.
+  # In particular, any terms that do not belong to the same equation will have
+  # a zero value.
+  big.vcov <- vcov(obj)
+
+  # This is a complete list of the terms. This is largely ignored, aside from
+  # the fact that we need a list of the formulae. In general, terms.multiple
+  # produced a pretty unwieldy list of items.
+  all.terms <- terms(obj)
+
+  # This list stores the results
+  simulations.list <- list()
+
+  # Iterate through the set of terms, and simulate each list separately.
+  for (key in names(all.terms)) {
+
+    # Extract the terms for an individual model.
+    eq.terms <- terms(all.terms[[key]])
+
+    # Extract the labels for the terms
+    eq.term.labels <- attr(eq.terms, "term.labels")
+
+    # Add the labeled for the intercept column, if it should exist
+    if (attr(eq.terms, "intercept"))
+      eq.term.labels <- c("(Intercept)", eq.term.labels)
+
+    # Format the title, this should look like:
+    #   <list-item-name>_<term-label>
+    #
+    # So for the list: list(mu1 = y ~ x + sin(x))
+    # We get:
+    #   "mu1_(Intercept)" "mu1_x" "mu1_sin(x)"
+    entries <- paste(key, eq.term.labels, sep = "_")
+
+    # Extract the mean-value of this term (from the lumped-toegether vector)
+    eq.coef <- big.coef[entries]
+
+    # Extract the vcov matrix of this term (from the lumped-together matrix)
+    eq.vcov <- big.vcov[entries, entries]
+
+    # Simulate the parameters
+    eq.simulations <- mvrnorm(num, eq.coef, eq.vcov)
+
+    # Name the columns
+    colnames(eq.simulations) <- eq.term.labels
+
+    # Add to the list
+    simulations.list[[key]] <- eq.simulations
+
+  }
+
+
+  # Return.
+  list(
+       coef = simulations.list,
+       linkinv = NULL
+       )
+}
+
+#' @S3method qi twosls
+qi.twosls <- function(obj, x=NULL, x1=NULL, y=NULL, num=1000, param=NULL) {
+
+  # Compute the expected value of multistage LS methods
+  compute.ev <- function (x, param, terms) {
+
+    #
+    if (is.null(x) || is.na(x)) {
+      return(NA)
+    }
+
+    # Cast as data.frame
+    x <- as.data.frame(x)
+
+    # If 'x' has too many rows, there will currently be errors. This is an issue
+    # in Zelig-core
+    if (nrow(x) > 1) {
+      warning("This package does not currently support pooled results.")
+      x <- x[1, ]
+    }
+
+  }
+
+
+  # Begin regular function
+  terms <- terms(obj)
+
+  # :q
+  coef.list <- coef(param)
+
+  # Hold Results
+  eta <- list()
+
+  #
+  for (key in names(coef.list)) {
+    #
+    coef <- coef.list[[key]]
+    # print(colnames(coef))
+    small.x <- as.matrix(x$matrix[, colnames(coef)])
+    #
+    eta[[key]] <- coef %*% (small.x)
+  }
+
+
+  # Convert list into a matrix
+  eta <- Reduce(function (x, y) cbind(x, y), eta)
+
+  # Name each column after the associated equation
+  colnames(eta) <- names(terms)
+
+  # Return the results
+  list(
+       "Expected Value: E(Y|X)" = eta
+       )
+}
+
+#' @S3method describe twosls
+describe.twosls <- function (...) {
+  list(
+       authors = c("Ferdinand Alimadhi", "Ying Lu", "Elena Villalon"),
+       description = "Two Stage Least Squares",
+       year = 2007
+       )
+}
+
+#' @export
+callsystemfit<-function(formula,data,method,inst=NULL,...){
+  # Call systemfit..
+  out <- systemfit(
+                   data = data,
+                   formula = formula,
+                   method = method,
+                   inst = inst,
+                   ...
+                   )
+
+  # Assign class to formula, so that it is correctly parsed
+  class(formula) <- c("multiple", "list")
+  
+  # Set the terms explicitly
+  attr(out,"terms") <- terms(formula)
+
+  # Set the class explicitly
+  class(out) <- c("multiple", class(out))
+
+  # Fin. Return the modified object
+  return(out)
+}
