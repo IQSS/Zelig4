d37c629d02f7e2aa6e4c7d9ee7c284fa78fc4405
tercer <jhonaker@iq.harvard.edu>
2013-10-17 16:27:39 -0400

    further solving environment issues with setx

--- tests/models-core.R
+++ tests/models-core.R
@@ -52,8 +52,8 @@ plot(s)
 
 z <- zelig(unem ~ gdp + capmob + trade, model = "ls", data = macro)
 
-x.high <- setx(z, trade = quantile(trade, 0.8))
-x.low <- setx(z, trade = quantile(trade, 0.2))
+x.high <- setx(z, trade = quantile(macro$trade, 0.8))
+x.low <- setx(z, trade = quantile(macro$trade, 0.2))
 
 s <- sim(z, x = x.high, x1 = x.low, num = 10)
 
@@ -77,8 +77,8 @@ plot(s)
 
 z <- zelig(unem ~ gdp + capmob + trade, model = "normal", data = macro)
 
-x.high <- setx(z, trade = quantile(trade, 0.8))
-x.low <- setx(z, trade = quantile(trade, 0.2))
+x.high <- setx(z, trade = quantile(macro$trade, 0.8))
+x.low <- setx(z, trade = quantile(macro$trade, 0.2))
 
 s <- sim(z, x = x.high, x1 = x.low)
 
